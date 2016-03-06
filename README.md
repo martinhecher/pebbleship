6c7e74bea5e8bcb0549f494dc74f382be6aca98f
Martin Hecher <martin.hecher@fraunhofer.at>
2016-03-06 10:34:22 +0000

    added development script for code live reloading

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # pebbleship-server
 
-a [Sails](http://sailsjs.org) application
+
+## Development setup
+
+To start a development server with reloading functionality use ```npm run dev```. It uses ```nodemon``` as a prerequisite which has to be installed with ```npm install -g nodemon``` first.
+
+## Notes
+
+* The used API framework [Sails](http://sailsjs.org) only supports a subset of ES6 features available in NodeJS 4.x. Block-scoped declaratione (let, const, class, etc.) are not supported. That's the reason for still using 'var' instead of 'let'.
--- api/controllers/GridController.js
+++ api/controllers/GridController.js
@@ -6,6 +6,24 @@
  */
 
 module.exports = {
-	
-};
+	create(req, res, next) {
+		var config = {
+			x: +req.body.x,
+			y: +req.body.y
+		}
 
+		if (!config.x || !config.y) {
+			console.log('[pebbleship-server] Invalid request: %s', JSON.stringify(config, null, 4));
+			return res.send('Please specify the rows and cols of the grid as "x" and "y" in the request!').status(500);
+		}
+
+		console.log('[pebbleship-server] Reqesting new grid: %s', JSON.stringify(config, null, 4));
+
+		// Application only supports a single grid at the moment
+		Grid.destroy({}).then(() => {
+			Grid.create(config).then(grid => {
+				console.log('[pebbleship-server] Created new grid, ready to play!');
+			})
+		});
+	}
+};
